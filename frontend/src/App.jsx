7b8d9200cee4580ee63b65a0c7b1d14968e3c0f2
tsunflowerr <23020155@vnu.edu.vn>
2025-08-01 17:33:50 +0700

    Navbar frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,61 @@
 import React from "react"
-
+import { Outlet, Route, useAsyncValue } from "react-router-dom"
+import Layout from "./components/Layout"
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
 const App = () => {
+
+  const navigate = useNavigate()
+  const [currentUser, setCurrentUser] = useState(() => {
+    const stored = localStorage.getItem("currentUser")
+    return stored ? JSON.parse(stored) : null
+  });
+
+  useEffect(() => {
+    if(currentUser) {
+      localStorage.setItem("currentUser", JSON.stringify(currentUser))
+    }
+    else {
+      localStorage.removeItem("currentUser")
+    }
+  }, [currentUser])
+
+  const handleAuthSubmit = data => {
+    const user = {
+      email: data.email,
+      name: data.name || "User",
+      avatar: `https://ui-avatars.com/api/?name=${encodeURIComponent(data.name || "User")}&background=random`
+    }
+    setCurrentUser(user);
+    navigate('/', { replace: true })
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+    setCurrentUser(null)
+    navigate('/login', { replace: true })
+  }
+
+  const PortectedLayout = () => {
+    <Layout user={currentUser} onLogout={handleLogout}>
+      <Outlet />
+    </Layout>
+  }
+
   return (
-    <div>
-      <h1 className ="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/login' element={<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center
+      justify-center">
+        <Login onSubmit={handleAuthSubmit} onSwitchMode ={() => nevigate('/signup')}/>
+      </div>} />
+
+      <Route path='/signup' element={<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center
+      justify-center">
+        <SignUp onSubmit={handleAuthSubmit} onSwitchMode ={() => nevigate('/login')}/>
+      </div>} />
+
+      <Route path='/' element={<Layout />} />
+    </Routes>
   )
 }
 
